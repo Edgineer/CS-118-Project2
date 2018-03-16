c2eca44b3440d683887bbdfc4781503336017c15
Edgar Lopez <Edgineer@users.noreply.github.com>
2018-03-15 21:08:51 -0700

    Add files via upload

--- SRclient.c
+++ SRclient.c
@@ -16,14 +16,14 @@ struct TCP_PACKET rcv_buffer[5];
 //hold the packet, because it contains the sequence number of the data received
 
   //if the packets sequence number is < rcv_base, it is a duplicate
-    //just send an ACK and do nothing to the buffer (possibly check for unwritten in order packets)
+    //just send an ACK and do nothing to the buffer
   
   //else, this is a new packet and we must loop through the buffer
-    //look for a sequence number == rcv_base
-        //if you find one write the data to the file and increment the rcv base by amount of data
-
     //iterate through the packets to find one with the sequence number < rcv_base
       //replace this index with the new packet
+
+  //look for a sequence number == rcv_base
+      //if you find one write the data to the file and increment the rcv base by amount of data
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -150,7 +150,7 @@ int main(int argc, char *argv[]) {
     fprintf(stderr,"404 NOT FOUND\n"); //display 404 not found
   }
 
-  else (packet1.FILENOTFOUND_flag == 0) { //file was found and more packets might be coming
+  else if (packet1.FILENOTFOUND_flag == 0) { //file was found and more packets might be coming
 
     file_path = (char*)calloc(strlen(filename)+15,sizeof(char)); // allocate for complete filepath including directory (should check the return value)
     snprintf(file_path,15,"%s",dir);//copy dir name
@@ -161,7 +161,7 @@ int main(int argc, char *argv[]) {
     //Initialize the Buffer
     for (int i = 0; i < 5; ++i) {
       rcv_buffer[i].seq_num=-1;
-      bzero(rcv_buffer[i],DATA_SIZE);
+      bzero(rcv_buffer[i].data,DATA_SIZE);
     }
     
     int numDataLastPacket = strlen(packet1.data);
@@ -170,6 +170,14 @@ int main(int argc, char *argv[]) {
     fdArr[0].events = (POLLIN);//POLLIN+POLLHUP+POLLERR
     
     while(numDataLastPacket == DATA_SIZE-1) { //indicating the last packet had full data packet, assuming at least one more packet left. Not gauranteed
+      
+      for (int i = 0; i < 5; ++i) { //Iterate buffer and increase the rcv_base as needed
+        if (rcv_base == rcv_buffer[i].seq_num) { //the last unAcked packet has now been ACKed
+          rcv_base = rcv_buffer[i].seq_num + HEADERSIZE + strlen(rcv_buffer[i].data);
+          write(fd,rcv_buffer[i].data,strlen(rcv_buffer[i].data)); //write ordered data
+        }
+      }
+
       //use poll to wait for a packet, read the packet and create an ACK for it   
       poll(fdArr,1,0);//0 is the length of the timeout
       
@@ -190,14 +198,24 @@ int main(int argc, char *argv[]) {
  	        n = sendto(sockfd, &dupACK, PACKETSIZE, 0, &serveraddr, serverlen);
  	        if (n < 0) 
   	        error("ERROR in sendto for ACK Segment");
- 	        fprintf(stderr, "Sending packet %d Retransmission\n",ACK.ack_num);
+ 	        fprintf(stderr, "Sending packet %d Retransmission\n",dupACK.ack_num);
  	        numDataLastPacket = strlen(packet.data);
- 	        fprintf(stderr, "numDataLastPacket:%d\n",strlen(packet.data));
  	      }
 
  	      else { //client is receiving new data so save it and update the rcv_base
- 	        write(fd,packet.data,strlen(packet.data)); //write the data to the packet, we are assuming reliable delivery of in order packets
- 	        //send ACK Packet
+            //this is a new packet and we must loop through the buffer
+              //iterate through the packets to find one with the sequence number < rcv_base
+              //replace this index with the new packet
+ 	        for (int i = 0; i < 5; ++i) {
+            if (rcv_buffer[i].seq_num < rcv_base) { //found old packet
+              rcv_buffer[i].seq_num = packet.seq_num;
+              rcv_buffer[i].ack_num = packet.ack_num;
+              //maybe add the other member variables
+              bzero(rcv_buffer[i].data,DATA_SIZE);//Sure the data was already written to the file?
+              snprintf(rcv_buffer[i].data,strlen(packet.data),"%s",packet.data);
+            }
+          }
+          //send ACK Packet
  	        struct TCP_PACKET ACK;
  	        ACK.ack_num = packet.seq_num + HEADERSIZE + strlen(packet.data);
  	        bzero(ACK.data,DATA_SIZE);
@@ -207,15 +225,7 @@ int main(int argc, char *argv[]) {
   	        error("ERROR in sendto for ACK Segment"); 	    
  	        fprintf(stderr, "Sending packet %d\n",ACK.ack_num);
  	        numDataLastPacket = strlen(packet.data);
- 	        fprintf(stderr, "numDataLastPacket:%d\n",strlen(packet.data));
-		      rcv_base = ACK.ack_num;
 		    }
-      }
-      for (int i = 0; i < 5; ++i) { //Iterate buffer and increase the rcv_base as needed
-        if (rcv_base == rcv_buffer[i].seq_num) { //the last unAcked packet has now been ACKed
-          rcv_base = rcv_buffer[i].seq_num + HEADERSIZE + strlen(rcv_buffer[i].data);
-          write(fd,rcv_buffer[i].data,strlen(rcv_buffer[i].data)); //write ordered data
-        }
       }
     }
     close(fd);
@@ -228,6 +238,5 @@ int main(int argc, char *argv[]) {
   *
   *
   */
-
  return 0;
 }
